21a713d4b5c8be5e00e7f246c9b4d19e3fbb92da
117256731 <117256731@qq.com>
2024-08-19 19:48:11 +1000

    Update zhuyuan.py

--- pyzzz/agents/zhuyuan.py
+++ pyzzz/agents/zhuyuan.py
@@ -68,13 +68,13 @@ class Zhuyuan(Agent):
     
     def Dash(self):
         value = self.dash["dmg"] + self.dash["dmg_grow"] * (
-            self.skill_levels.dogde - 1
+            self.skill_levels.dodge - 1
         )
         return Attack(AttackKind.Dash, Attribute.Ether, value)
 
     def Dodge(self):
         value = self.dogde["dmg"] + self.dogde["dmg_grow"] * (
-            self.skill_levels.dogde - 1
+            self.skill_levels.dodge - 1
         )
         return Attack(AttackKind.Dodge, Attribute.Ether, value)
     
@@ -139,12 +139,14 @@ class Zhuyuan(Agent):
             source="Zhuyuan rep6 skill",
         )
 
-    def buffs(self, _: bool = True):
-        res = [self.core_skill(), self.extra_skill()]
+    def buffs(self, context: ContextData | None = None):
+        res = [self.core_skill1(), self.core_skill2(), self.extra_skill()]
         if self._repetition >= 2:
             res.append(self.rep2())
         if self._repetition >= 4:
             res.append(self.rep4())
+        # if context and context.agent == self.name and self._repetition >= 6:
         if self._repetition >= 6:
             res.append(self.rep6())
         return res
+
