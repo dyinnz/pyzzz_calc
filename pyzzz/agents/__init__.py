9319fcb6ba0a1e8caadd45e8454d1e0b2d42bdd8
117256731 <117256731@qq.com>
2024-08-19 13:24:10 +1000

    Update __init__.py

--- pyzzz/agents/__init__.py
+++ pyzzz/agents/__init__.py
@@ -3,6 +3,10 @@ from pyzzz.agents.ellen import Ellen
 from pyzzz.agents.grace import Grace
 from pyzzz.agents.lycaon import Lycaon
 from pyzzz.agents.soukaku import Soukaku
+from pyzzz.agents.rina import Rina
+from pyzzz.agents.nekomata import Nekomata
+from pyzzz.agents.soldier11 import Soldier11
+from pyzzz.agents.zhuyuan import Zhuyuan
 
 
 def create_agent(name: str, **kw):
@@ -11,6 +15,10 @@ def create_agent(name: str, **kw):
         "Lycaon": Lycaon,
         "Soukaku": Soukaku,
         "Grace": Grace,
+        "Rina": Rina,
+        "Nekomata" : Nekomata,
+        "Soldier11": Soldier11,
+        "Zhuyuan": Zhuyuan,
     }[
         name
     ](**kw)
