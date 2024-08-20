c90cfda43ec48986f6c3e52aaacc76c5ca038d9a
117256731 <117256731@qq.com>
2024-08-20 23:04:56 +1000

    Update nicole.py

--- pyzzz/agents/nicole.py
+++ pyzzz/agents/nicole.py
@@ -33,19 +33,31 @@ class Nicole(Agent):
 
         self.core_skill_atk = int(core_skill_atk) if core_skill_atk else None
 
-        self.e1 = self._skill["特殊技：糖衣炮弹"]
+        self.e1 = self._skill["特殊技：糖衣炮弹-A"]
+        self.e2 = self._skill["特殊技：糖衣炮弹-B"]
 
-        self.ex1 = self._skill["强化特殊技：夹心糖衣炮弹-蓄力伤害"]
-        self.ex2 = self._skill["强化特殊技：夹心糖衣炮弹-炮击伤害"]
-        self.ex3 = self._skill["强化特殊技：夹心糖衣炮弹-能量场伤害"]
+        self.ex1 = self._skill["强化特殊技：夹心糖衣炮弹-蓄力"]
+        self.ex2 = self._skill["强化特殊技：夹心糖衣炮弹-炮击A"]
+        self.ex3 = self._skill["强化特殊技：夹心糖衣炮弹-炮击B"]
+        self.ex4 = self._skill["强化特殊技：夹心糖衣炮弹-能量场"]
 
-        self.dogde = self._skill["闪避反击：牵制炮击-"]
-        self.chain = self._skill["连携技：高价以太爆弹-"]
-        self.final = self._skill["终结技：特制以太榴弹-"]
+        self.dogde1 = self._skill["闪避反击：牵制炮击-A"]
+        self.dogde2 = self._skill["闪避反击：牵制炮击-B"]
+
+        self.chain1 = self._skill["连携技：高价以太爆弹-炮击A"]
+        self.chain2 = self._skill["连携技：高价以太爆弹-炮击B"]
+        self.chain3 = self._skill["连携技：高价以太爆弹-能量场"]
+        self.final1 = self._skill["终结技：特制以太榴弹-炮击"]
+        self.final2 = self._skill["终结技：特制以太榴弹-能量场"]
 
     def E1(self):
         value = self.e1["dmg"] + self.e1["dmg_grow"] * (self.skill_levels.special - 1)
         return Attack(AttackKind.Special, Attribute.Ether, value)
+
+    def E2(self):
+        value = self.e2["dmg"] + self.e2["dmg_grow"] * (self.skill_levels.special - 1)
+        return Attack(AttackKind.Special, Attribute.Ether, value)
+
 
     def EX1(self):
         value = self.ex1["dmg"] + self.ex1["dmg_grow"] * (self.skill_levels.special - 1)
@@ -59,23 +71,55 @@ class Nicole(Agent):
         value = self.ex3["dmg"] + self.ex3["dmg_grow"] * (self.skill_levels.special - 1)
         return Attack(AttackKind.SpecialEx, Attribute.Ether, value)
 
-    def Dodge(self):
-        value = self.dogde["dmg"] + self.dogde["dmg_grow"] * (
+    def EX4(self):
+        value = self.ex4["dmg"] + self.ex4["dmg_grow"] * (self.skill_levels.special - 1)
+        return Attack(AttackKind.SpecialEx, Attribute.Ether, value)
+
+
+    def Dodge1(self):
+        value = self.dogde1["dmg"] + self.dogde1["dmg_grow"] * (
             self.skill_levels.dodge - 1
         )
         return Attack(AttackKind.Dodge, Attribute.Ether, value)
 
-    def Chain(self):
-        value = self.chain["dmg"] + self.chain["dmg_grow"] * (
+    def Dodge2(self):
+        value = self.dogde2["dmg"] + self.dogde2["dmg_grow"] * (
+            self.skill_levels.dodge - 1
+        )
+        return Attack(AttackKind.Dodge, Attribute.Ether, value)
+
+
+    def Chain1(self):
+        value = self.chain1["dmg"] + self.chain1["dmg_grow"] * (
             self.skill_levels.chain - 1
         )
         return Attack(AttackKind.Chain, Attribute.Ether, value)
 
-    def Final(self):
-        value = self.final["dmg"] + self.final["dmg_grow"] * (
+    def Chain2(self):
+        value = self.chain2["dmg"] + self.chain2["dmg_grow"] * (
+            self.skill_levels.chain - 1
+        )
+        return Attack(AttackKind.Chain, Attribute.Ether, value)
+
+    def Chain3(self):
+        value = self.chain3["dmg"] + self.chain3["dmg_grow"] * (
+            self.skill_levels.chain - 1
+        )
+        return Attack(AttackKind.Chain, Attribute.Ether, value)
+
+
+    def Final1(self):
+        value = self.final1["dmg"] + self.final1["dmg_grow"] * (
             self.skill_levels.chain - 1
         )
         return Attack(AttackKind.Final, Attribute.Ether, value)
+
+    def Final2(self):
+        value = self.final2["dmg"] + self.final2["dmg_grow"] * (
+            self.skill_levels.chain - 1
+        )
+        return Attack(AttackKind.Final, Attribute.Ether, value)
+
 
     def core_skill(self):
         def create():
