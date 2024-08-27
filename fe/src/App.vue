b817bcb73f6c2974565ca38857b75bce03ab5c7e
dyinnz <dyinnz@gmail.com>
2024-08-27 21:23:15 +0800

    update fe

---
+++ fe/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <!-- <h1>ZZZ Calculator</h1> -->
+
+  <!-- <el-button>Hello</el-button> -->
+
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="80px">Aside</el-aside>
+      <el-main>
+        <div>
+          Agent
+          <el-select v-model="agentName">
+            <el-option
+              v-for="item in options"
+              :label="item.value"
+              :value="item.value"
+            />
+          </el-select>
+
+          <el-input-number v-model="agentRep" :min="0" :max="6">
+          </el-input-number>
+
+          <el-input-number v-model="agentLevel" :min="1" :max="60">
+          </el-input-number>
+
+          <el-select v-model="weaponName">
+            <el-option
+              v-for="item in options"
+              :label="item.value"
+              :value="item.value"
+            />
+          </el-select>
+          <el-input-number v-model="weaponRep" :min="1" :max="60">
+          </el-input-number>
+
+          <el-input-number v-model="weaponLevel" :min="1" :max="60">
+          </el-input-number>
+        </div>
+        <textarea v-model="agentName"></textarea>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const agentName = ref("Ellen");
+const agentRep = ref(0);
+const agentLevel = ref(60);
+
+const weaponName = ref("Ellen");
+const weaponRep = ref(0);
+const weaponLevel = ref(60);
+
+const options = [
+  {
+    value: "Ellen",
+  },
+  {
+    value: "Lycaon",
+  },
+  {
+    value: "Soukaku",
+  },
+];
+</script>
---
+++ fe/vite.renderer.config.ts
@@ -0,0 +1,38 @@
+import type { ConfigEnv, UserConfig } from "vite";
+import { defineConfig } from "vite";
+import { pluginExposeRenderer } from "./vite.base.config";
+import vue from "@vitejs/plugin-vue";
+
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
+// https://vitejs.dev/config
+export default defineConfig((env) => {
+  const forgeEnv = env as ConfigEnv<"renderer">;
+  const { root, mode, forgeConfigSelf } = forgeEnv;
+  const name = forgeConfigSelf.name ?? "";
+
+  return {
+    root,
+    mode,
+    base: "./",
+    build: {
+      outDir: `.vite/renderer/${name}`,
+    },
+    plugins: [
+      vue(),
+      pluginExposeRenderer(name),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    resolve: {
+      preserveSymlinks: true,
+    },
+    clearScreen: false,
+  } as UserConfig;
+});
